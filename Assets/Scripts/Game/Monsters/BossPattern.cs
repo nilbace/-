fdde049f83b51290abf4ae89fb0c8e34b135c9a3
nilbace <101455214+nilbace@users.noreply.github.com>
2023-07-11 22:55:19 +0900

    원숭이까지 완료

--- Assets/Scripts/Game/Monsters/BossPattern.cs
+++ Assets/Scripts/Game/Monsters/BossPattern.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BossPattern : MonoBehaviour
 {
@@ -136,12 +137,204 @@ public class BossPattern : MonoBehaviour
                         ShootLongBullet(PatternData[9], new Vector3(2f, 0f, 0f));
                         
                         break;
-                        #endregion
+                    
 
+                    case 14:   //1번 패턴
+                        ShootBullet(PatternData[0], new Vector3(-1f, -0f, 0f));
+                        ShootBullet(PatternData[1], new Vector3(1f, -0f, 0f));
+                        yield return new WaitForSeconds(1f);
+                        ShootBullet(PatternData[0], new Vector3(0.5f, -0f, 0f));
+                        ShootBullet(PatternData[1], new Vector3(-0.5f, -0f, 0f));
+
+                        break;
+
+
+                    case 15:    //3번 패턴
+                        transform.DOMoveX(-1f, 1f);
+                        yield return new WaitForSeconds(1f);
+                        //왼쪽으로 이동
+
+                        for (int j = 0; j < 5; j++)
+                        {
+                            Pattern6();
+                            yield return new WaitForSeconds(PatternDelay[4]);
+                        }
+                        yield return new WaitForSeconds(2f);
+                        //발사 + 대기까지 2초
+
+                        transform.DOMoveX(0f, 1f);
+                        yield return new WaitForSeconds(1f);
+                        //돌아오기
+
+                        break;
+
+                    case 16:    //4번 패턴
+                        transform.DOMoveX(1f, 1f);
+                        yield return new WaitForSeconds(1f);
+                        //왼쪽으로 이동
+
+                        for (int j = 0; j < 5; j++)
+                        {
+                            Pattern6();
+                            yield return new WaitForSeconds(PatternDelay[4]);
+                        }
+                        yield return new WaitForSeconds(2f);
+                        //발사 + 대기까지 2초
+
+                        transform.DOMoveX(0f, 1f);
+                        yield return new WaitForSeconds(1f);
+                        //돌아오기
+
+                        break;
+                    #endregion
+
+                    #region Monkey
+                    case 17:  //X자 공격
+                        Vector3 offset = new Vector3(1.4f, 0, 0);
+
+                        for(int j = 0; j < 5; j++)
+                        {
+                            ShootBullet(PatternData[10], (-2f +j)*offset);
+                            ShootBullet(PatternData[10], (2f - j) * offset);
+                            yield return new WaitForSeconds(0.5f);
+                        }
+                   break;
+
+                    case 18: //왼쪽부터 이동하면서 공격
+                        for(int k = -1; k <=1; k++)
+                        {
+                            float poz = k;
+                            transform.DOMoveX(poz, 1f);
+                            yield return new WaitForSeconds(1f);
+
+                            for (int j = -1; j <= 1; j++)
+                            {
+                                float offset2;
+                                if (j == -1 || j == 1) offset2 = 0.5f;
+                                else offset2 = 1f;
+
+                                ShootBullet(PatternData[10], new Vector3(offset2, 0, 0));
+                                ShootBullet(PatternData[10], new Vector3(-offset2, 0, 0));
+                                yield return new WaitForSeconds(0.5f);
+                            }
+                        }
+                        transform.DOMoveX(0, 1f);
+                        yield return new WaitForSeconds(1f);
+                        break;
+
+                    case 19: //왼쪽부터 이동하면서 공격
+                        for(int j =0; j<3;j++)
+                        {
+                            ShootLongBullet(PatternData[9], new Vector3(-2f, 0f, 0f));
+                            ShootLongBullet(PatternData[9], new Vector3(2f, 0f, 0f));
+                            yield return new WaitForSeconds(1f);
+                        }
+                        break;
+
+                    case 20:
+                        transform.DOMoveX(-1.5f, 0.5f);
+                        yield return new WaitForSeconds(0.5f);
+                        transform.DOMoveX(1.5f, 1f);
+                        for (int j = 0; j < 5; j++)
+                        {
+                            ShootBullet(PatternData[10]);
+                            ShootBullet(PatternData[10], new Vector3(0,-1f,0));
+                            yield return new WaitForSeconds(0.2f);
+                        }
+                        transform.DOMoveX(0f, 0.5f);
+                        yield return new WaitForSeconds(0.5f);
+                        break;
+
+                    case 21:
+                        transform.DOMoveX(1.5f, 0.5f);
+                        yield return new WaitForSeconds(0.5f);
+                        transform.DOMoveX(-1.5f, 1f);
+                        for (int j = 0; j < 5; j++)
+                        {
+                            ShootBullet(PatternData[10]);
+                            ShootBullet(PatternData[10], new Vector3(0, -1f, 0));
+                            yield return new WaitForSeconds(0.2f);
+                        }
+                        transform.DOMoveX(0f, 0.5f);
+                        yield return new WaitForSeconds(0.5f);
+                        break;
+                    
+
+                    case 22:  //원형으로 발사
+                        transform.DOMoveY(0.5f, 0.5f);
+                        yield return new WaitForSeconds(0.5f);
+
+                        float angle2 = 18f;
+                        for (int j = 0; j < 10; j++)
+                        {
+                            Vector3 dir1 = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle2), Mathf.Sin(Mathf.Deg2Rad * angle2), 0f);
+
+                            GameObject bullet = Instantiate(BulletPrefab);
+                            Vector3 temp = transform.position + new Vector3(0, -1, 0);
+                            bullet.GetComponent<MonsterBullet>().Setting(dir1, 3f, temp);
+                            bullet.transform.localScale = Vector3.one * 1f;
+
+
+
+                            angle2 -= 36f;
+                        }
+                        yield return new WaitForSeconds(0.5f);
+
+                        transform.DOMoveY(3.5f, 0.5f);
+                        yield return new WaitForSeconds(0.5f);
+                        break;
+
+                    case 23:
+
+                        Vector3[] _pozs = new Vector3[]
+                        {
+                            new Vector3(-1, 0, 0),
+                            new Vector3(0, 0.5f, 0),
+                            new Vector3(1, 0, 0)
+                        };
+
+                        for (int j =0; j <3; j++)
+                        {
+                            GameObject mon2 = Instantiate(gameObject, transform.position, Quaternion.identity);
+                            Vector3 temppoz = _pozs[j];
+                            Color tempColor = mon2.GetComponent<SpriteRenderer>().color;
+                            tempColor.r = tempColor.g = tempColor.b = 0.4f;
+                            mon2.GetComponent<SpriteRenderer>().color = tempColor;
+                            mon2.transform.DOMove(temppoz, 1f);
+                            Destroy(mon2, 2f);
+                            yield return new WaitForSeconds(1f);
+                            mon2.GetComponent<BossPattern>().MonkeyClone();
+                        }
+                        break;
+
+                        #endregion
                 }
                 yield return new WaitForSeconds(pigPattern[i].y);
             }
         }
+    }
+
+    public void MonkeyClone()
+    {
+        StopAllCoroutines();
+        StartCoroutine(MonkeyCloneSkill());
+    }
+
+    IEnumerator MonkeyCloneSkill()
+    {
+        float angle2 = 18f;
+        for (int j = 0; j < 10; j++)
+        {
+            Vector3 dir1 = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle2), Mathf.Sin(Mathf.Deg2Rad * angle2), 0f);
+
+            GameObject bullet = Instantiate(BulletPrefab);
+            Vector3 temp = transform.position + new Vector3(0, -1, 0);
+            bullet.GetComponent<MonsterBullet>().Setting(dir1, 3f, temp);
+            bullet.transform.localScale = Vector3.one * 1f;
+
+            angle2 -= 36f;
+        }
+        yield return new WaitForSeconds(0.5f);
     }
     
 
